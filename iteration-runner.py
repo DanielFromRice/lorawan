10a4f7e26f1538a0c00431736b27bd4bd79613fc
Daniel Rothfusz <dpr7@alumni.rice.edu>
2026-05-30 22:48:32 -0700

    Split iteration runner to break out by node count

--- iteration-runner.py
+++ iteration-runner.py
@@ -1,32 +1,46 @@
 import subprocess
 import time
+import argparse
 
-tx_periods = [1, 2, 5, 10, 15, 20]
-node_counts = [[50, 0], [45, 5], [40, 10], [35, 15], [30, 20], [25, 25], [20, 30], [15, 35], [10, 40], [5, 45], [0, 50]]
-pktsizes_b = [400, 1000, 1600, 2200, 2800, 3400, 4000]
+tx_periods = [2*60, 5*60, 10*60, 15*60, 20*60] # Reported to program in seconds
+node_counts = [[50, 0], [40, 10], [30, 20], [20, 30], [10, 40], [0, 50]]
+pktsizes_b = [500, 1000, 2000, 3000, 4000]
 seeds = [1, 2, 3, 4, 5]
 
-output_file_name = "../../cumulative_results.csv"
+parser = argparse.ArgumentParser()
+
+# Enables manual multiprocessing by batching over node counts
+parser.add_argument("-n", "--run_number", type=int, default=0, required=True)
+args = parser.parse_args()
+
+if args.run_number > len(node_counts) or args.run_number < 0:
+    print("Invalid run number")
+    exit(1)
+
+output_file_name = "../../cumulative_results_" + str(args.run_number) + ".csv"
+
 iteration = 0
+
+print("Running with run number " + str(args.run_number))
 
 with open(output_file_name, 'a') as outfile:
     outfile.write(time.asctime())
     outfile.write("\nperiodA, nodesA, sentA, recvA, lossA, periodB, nodesB, sentB, recvB, lossB, seed, pktsizeB\n")
 
     for tx_period in tx_periods:
-        for count_a, count_b in node_counts:
-            for pktsize_b in pktsizes_b:
-                for seed in seeds:
-                    command = f"../../ns3 run \"wes-simulation --appPeriodA={tx_period} --appPeriodB={tx_period}" \
-                              f" --nDevicesA={count_a} --nDevicesB={count_b} --packetSizeB={pktsize_b} --seed={seed}\""
+        for pktsize_b in pktsizes_b:
+            for seed in seeds:
+                command = f"../../ns3 run --no-build \"wes-simulation --appPeriodA={tx_period} --appPeriodB={tx_period}" \
+                          f" --nDevicesA={node_counts[args.run_number][0]} --nDevicesB={node_counts[args.run_number][1]}"\
+                          f" --packetSizeB={pktsize_b} --seed={seed}\""
 
-                    iteration += 1
-                    print(iteration, command)
-                    result = subprocess.run(command,capture_output=True,text=True,shell=True)
+                iteration += 1
+                print(iteration, command)
+                result = subprocess.run(command,capture_output=True,text=True,shell=True)
 
-                    if result.returncode != 0:
-                        print(result.stderr)
+                if result.returncode != 0:
+                    print(result.stderr)
 
-                    outfile.write(result.stdout)
+                outfile.write(result.stdout)
 
             outfile.flush()
